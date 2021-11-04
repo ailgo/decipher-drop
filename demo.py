ee2745a2c15a42b917cd1745614de7d7165d22ac
Ben Guidarelli <ben.guidarelli@gmail.com>
2021-11-04 10:56:23 -0400

    add files

---
+++ demo.py
@@ -0,0 +1,27 @@
+#e2e demo of use
+from nftcreate import *
+from seed import *
+from claim import *
+from recover import *
+
+N=5
+pw_length=5
+
+genesis_accts = get_accounts()
+[seeder, seeder_key] = genesis_accts[0]
+print("Using {} to create nfts and seed accounts ".format(seeder))
+
+print ("Creating {} dummy NFTs".format(N))
+nftIds = createNFTs(seeder, seeder_key, N)
+print("Created {}".format(nftIds))
+
+print("Creating escrow accounts to hold nfts")
+initialize_accounts(seeder, seeder_key, nftIds, pw_length)
+
+print("Claiming {} NFTs".format(N-1))
+pws = get_passwords()
+for idx in range(N-1):
+    simulate_claim(seeder, pws[idx], nftIds[idx])
+
+#print("Recovering unclaimed NFT")
+#recover(seeder, seeder_key, nftIds[-1])
